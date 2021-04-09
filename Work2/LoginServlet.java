ba9c0a2d4783322347abf124cf2f9299e7318741
shk001 <50333579+shk314159@users.noreply.github.com>
2021-04-09 23:51:27 +0800

    ad

--- Work2/LoginServlet.java
+++ Work2/LoginServlet.java
@@ -9,7 +9,6 @@ public class LoginServlet extends HttpServlet {
 	throws ServletException, IOException{
 
 		String username = req.getParameter("username");
-
 		String password = req.getParameter("password");
 
 		if("admin".equals(username) && "123456".equals(password)) {
@@ -17,13 +16,10 @@ public class LoginServlet extends HttpServlet {
 			ServletContext context = getServletContext();
 
 			RequestDispatcher dispatcher = context.getNamedDispatcher("dispatcher");
-
+			System.out.println("test success");
 			dispatcher.forward(req, res);
-
 		}else {
-
 			throw new RuntimeException("Login failed.");
-
 		}
 	}
 
--- Work2/TestLoginServlet.java
+++ Work2/TestLoginServlet.java
@@ -5,11 +5,10 @@ import org.easymock.EasyMock;
 import org.junit.Before;
 import org.junit.jupiter.api.*;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.*;
 import java.io.IOException;
 
 import static org.easymock.EasyMock.*;
@@ -20,35 +19,57 @@ import static org.junit.platform.commons.function.Try.success;
 public class TestLoginServlet {
     public HttpServletRequest request;
     public LoginServlet servlet;
-    public HttpSession session;
-    public void init() {
+    @Test
+    public void testLoginSuccess() throws ServletException, IOException {
+        // create mock:
+        HttpServletRequest request = createMock(HttpServletRequest.class);
+        final ServletContext contextObj = createMock(ServletContext.class);
+        RequestDispatcher dispatcherObj = createMock(RequestDispatcher.class);
 
-        servlet = new LoginServlet();
-        request = EasyMock.createStrictMock(HttpServletRequest.class);
-        session = EasyMock.createStrictMock(HttpSession.class);
+         // set behavior:
+        expect(request.getParameter("username")).andReturn("admin");
+        expect(request.getParameter("password")).andReturn("123456");
+        expect(contextObj.getNamedDispatcher("dispatcher")).andReturn(
+                dispatcherObj);
+        dispatcherObj.forward(request, null);
+        // done!
+        replay(request);
+        replay(contextObj);
+        replay(dispatcherObj);
+        // test:
+        LoginServlet servlet = new LoginServlet() {
 
+            private static final long serialVersionUID = 1L;
+
+            public ServletContext getServletContext() {
+                return contextObj;
+            }
+        };
+        servlet.doPost(request, null);
+        // verify:
+        verify(request);
+        verify(contextObj);
+        verify(dispatcherObj);
     }
 
     @Test
-    public void test() {
-        init();
-        expect(request.getParameter("username")).andReturn("admin");
-        expect(request.getParameter("password")).andReturn("1234");
-        replay(request);
+    public void testLoginFailed() {
+        request = EasyMock.createMock(HttpServletRequest.class);
+        EasyMock.expect(request.getParameter("username")).andReturn("admin");
+        EasyMock.expect(request.getParameter("password")).andReturn("1236");
+        EasyMock.replay(request);
         try {
-
+            servlet = new LoginServlet();
             servlet.doPost(request, null);
-            System.out.println("access");
-//            success("No catch!");
-        }catch (RuntimeException re) {
-            assertEquals("fail login","Login failed.", re.getMessage());
+            fail("Not caught exception!");
+        } catch (RuntimeException re) {
+            assertEquals("Login failed.", re.getMessage());
         } catch (ServletException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
         verify(request);
-
     }
 
 }
